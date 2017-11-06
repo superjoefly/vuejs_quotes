d65f7606c9ec3d2b571630067428479808e9f6e6
superjoefly <superjoefly@gmail.com>
2017-11-05 23:19:07 -0700

    Changed some styles and changed textarea to input field.

--- src/components/AddQuote.vue
+++ src/components/AddQuote.vue
@@ -4,14 +4,13 @@
 
     <p class="w3-text-red">{{message}}</p>
 
-    <!-- Use v-model to bind the data property to the textarea field -->
-      <textarea class="w3-pale-yellow w3-round" placeholder="Enter up to 10 Quotes!" style="width: 100%" rows="3" v-model="newQuote" @keyup.enter="addQuote" @focus="message = ''"/>
+      <input class="w3-input" placeholder="Enter up to 10 Quotes!" style="width: 100%" v-model="newQuote" @keyup.enter="addQuote($event.target.value)" autofocus @focus="message = ''"/>
 
 
 
     <!-- Add a click listener to execute a method that emits an event and passes the 'newQuote' data -->
     <div class="w3-container">
-      <button class="w3-btn w3-blue w3-round w3-margin" @click="addQuote">Add Quote!</button>
+      <button class="w3-btn w3-blue w3-round w3-margin" @click="addQuote($event)">Add Quote!</button>
     </div>
 
 
@@ -29,11 +28,13 @@
 
     methods: {
       addQuote() {
-        if (this.newQuote !== '') {
+        if(this.newQuote == '') {
+          this.message = 'Enter some text to add a quote!';
+        } else {
+
           this.$emit('quoteAdded', this.newQuote);
           this.newQuote = '';
-        } else {
-          this.message = 'Enter some text to add a quote!';
+          this.message = '';
         }
       }
     }
@@ -41,13 +42,11 @@
 </script>
 
 <style>
-textarea {
-  background: url("http://i.stack.imgur.com/ynxjD.png") repeat-y;
-}
 
 @media all and (min-width: 800px) {
-  textarea {
+  input {
     width: 50% !important;
+    margin: auto;
   }
 }
 </style>
